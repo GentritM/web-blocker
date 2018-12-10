b8f06e6225f03b3d31b8ebb60eb5556fb0ed0b05
GentritM <genti_mehmeti@outlook.com>
2018-12-10 11:24:57 +0200

    web blocker

--- web_blocker.py
+++ web_blocker.py
@@ -1,15 +1,17 @@
 import time
 from datetime import datetime as dt
 
-host_temp= "hosts"
-host_path = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+host_temp= "hosts" """the testing location of hosts for windows, for testing the script"""
+host_path = "C:\\Windows\\System32\\drivers\\etc\\hosts"  """the real location path of hosts for windows"""
 redirect = "127.0.0.1"
+"""example of webpages you want to block"""
 web_list=["www.facebook.com","facebook.com","Yahoo.com","123movieshub.com","https://123movieshub.com/"]
+
 
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day,18) < dt.now() < dt(dt.now().year,
-    dt.now().month, dt.now().day,21,49):
+    dt.now().month, dt.now().day,21,49):      """The time of blocking websites, my example from 18:21, can be changed to whatever time you want """
         print("websites cannot be launched")
 
         with open(host_path, 'r+') as file:    #working hours
